9f9395959740ef54232ab2b0c8e57f144d9b9642
Rick Strahl <rstrahl@west-wind.com>
2018-08-16 01:40:11 -0700

    Update for new sidebar opener functions.

--- KavaDocsAddin/KavaDocsMenuHandler.cs
+++ KavaDocsAddin/KavaDocsMenuHandler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,30 +22,6 @@ namespace KavaDocsAddin
 
             var mi = Model.TopicsTree.Resources["MainMenuKavaDocsMenu"] as MenuItem;
             mi.DataContext = Model.TopicsTree.Model;
-
-            //var item = Model.TopicsTree.FindResource("MainMenuKavaDocsMenu");
-            //if (item == null)
-                
-
-            //var mi = new MenuItem
-            //{
-            //    Header = "_Kava Docs",                
-            //};
-            //mi.Items.Add(new MenuItem
-            //{
-            //    Header = "_Open Project",
-            //    Command = Model.Commands.OpenProjectCommand
-            //});
-            //mi.Items.Add(new MenuItem
-            //{
-            //    Header = "_Save Project",
-            //    Command = Model.Commands.SaveProjectCommand
-            //});
-            //mi.Items.Add(new MenuItem
-            //{
-            //    Header = "_Close Project",
-            //    Command = Model.Commands.CloseProjectCommand
-            //});
 
             // insert Item after MainMenuEdit item on Main menu
             Model.Addin.AddMenuItem(mi, "MainMenuTools",mode: 0);
